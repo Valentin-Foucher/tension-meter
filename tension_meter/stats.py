83d447eee4f134496db0e3a6e5110b87ced8ca4f
Valentin <valentin@oncrawl.com>
2020-12-27 20:59:15 +0100

    style: results screen

--- tension_meter/stats.py
+++ tension_meter/stats.py
@@ -1,4 +1,4 @@
 def show_results(codes):
-    print('\n\nResponses by status code:\n')
+    print('\n\n==========================\nResponses by status code:\n')
     for code, count in codes.items():
-        print(f'{code}: {count} times')
+        print(f'{code}: {count} times\n==========================\n')
