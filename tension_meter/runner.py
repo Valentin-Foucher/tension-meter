f8d36a5dfc806b0f3cf9ceb711a8fd11899b8de5
Valentin <valentin@oncrawl.com>
2021-01-09 16:11:09 +0100

    enh: make the async runner show a summary

--- tension_meter/runner.py
+++ tension_meter/runner.py
@@ -16,6 +16,7 @@ class Runner(abc.ABC):
         self.limit = limit
         self.verbose = verbose
         self.cpt = 0
+        self.is_not_over = None
 
     def has_reached_max_count_limit(self):
         self.cpt += 1
@@ -24,20 +25,35 @@ class Runner(abc.ABC):
     def has_reached_max_time_limit(self):
         return datetime.datetime.now() < self.limit
 
+    def add_response_code(self, code):
+        if code not in self.codes:
+            self.codes[code] = 0
+
+        self.codes[code] += 1
+
     def make_request(self):
         response = core.make_request(self.url,
                                      self.method,
                                      headers=self.headers,
                                      data=self.data,
                                      params=self.params)
-        if response.status_code not in self.codes:
-            self.codes[response.status_code] = 0
+        self.add_response_code(response.status_code)
 
-        self.codes[response.status_code] += 1
         return core.format_response(response, self.method, self.url, self.verbose)
 
-    @abc.abstractmethod
     def run(self):
+        try:
+            self._run()
+        except KeyboardInterrupt:
+            pass
+        except utils.RequestException as e:
+            print(str(e))
+            exit()
+        finally:
+            stats.show_results(self.codes)
+
+    @abc.abstractmethod
+    def _run(self):
         raise NotImplementedError()
 
 
@@ -47,14 +63,9 @@ class SyncRunner(Runner):
         self.is_not_over = self.has_reached_max_time_limit if isinstance(self.limit, datetime.datetime) \
             else self.has_reached_max_count_limit
 
-    def run(self):
-        try:
-            while self.is_not_over():
-                print(self.make_request())
-        except KeyboardInterrupt:
-            pass
-        finally:
-            stats.show_results(self.codes)
+    def _run(self):
+        while self.is_not_over():
+            print(self.make_request())
 
 
 def _async_make_request(runner):
@@ -63,7 +74,7 @@ def _async_make_request(runner):
     :param runner: AsyncRunner instance
     :return: result of make_request
     """
-    return runner.make_request()
+    return runner.make_request(), list(runner.codes.keys())[0]
 
 
 class AsyncRunner(Runner):
@@ -74,10 +85,15 @@ class AsyncRunner(Runner):
 
         self.is_not_over = self.has_reached_max_count_limit
 
-    def run(self):
+    def _run(self):
         mp.set_start_method('spawn')
         pool = mp.Pool(4)
         results = pool.map_async(_async_make_request, (self for _ in range(self.limit)))
         pool.close()
         pool.join()
-        print('\n'.join(results.get()))
+        output = ''
+        for result in results.get():
+            output = f'{output}\n{result[0]}'
+            self.add_response_code(result[1])
+
+        print(output)
